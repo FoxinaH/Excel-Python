4971c82f3dc74988f9adc5bf389d70235e4bb2cd
FoxinaH <fleurinehell@proton.me>
2024-06-09 00:35:06 +0200

    fix: dependencies automatically installs

--- data.py
+++ data.py
@@ -1,4 +1,17 @@
 # %%
+import subprocess
+import sys
+
+def install_requirements():
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to install dependencies: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    install_requirements()
+# %%
 import pandas as pd
 import openpyxl
 from openpyxl.chart import PieChart, Reference
